fc589bb25a47fc2775244c686ca5dc985b289d6c
Veeramahalakshmi <veeramahalakshmimv@gmail.com>
2025-04-08 16:33:02 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows external access (important for Render)
+    port: 3000       // Optional: Set your desired port
+  }
 })
