67423b80a6a94744032000ec5e1bf1ed1a4105de
Veeramahalakshmi <veeramahalakshmimv@gmail.com>
2025-04-08 16:36:05 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allows external access (important for Render)
-    port: 3000       // Optional: Set your desired port
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['cryptide.onrender.com'], // 👈 Add your Render domain here
   }
 })
