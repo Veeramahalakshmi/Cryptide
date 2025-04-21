34b1417b0056c68f020a3b24b8ddb63dd2e91bf4
Veeramahalakshmi <veeramahalakshmimv@gmail.com>
2025-04-21 19:04:06 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: ['cryptide.onrender.com'], // 👈 Add your Render domain here
+    allowedHosts: ['cryptidee.onrender.com'], // 👈 Add your Render domain here
   }
 })
